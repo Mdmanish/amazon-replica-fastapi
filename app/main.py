cc8ae0e786453c193c19981010dee3712b05bca8
Mdmanish <mdmanishurrahman7860@gmail.com>
2024-04-24 18:34:06 +0530

    db configuration changed to postgresql

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000',],
+    allow_origins=['http://localhost:3000','https://amazon-replica-manish.netlify.app'],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allow_headers=["*"],
